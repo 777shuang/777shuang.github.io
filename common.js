be491030e96d8792937f91cb8a8796a4d0f1e31c
777shuang <shuang777@outlook.jp>
2022-11-15 12:32:49 +0900

    Update

---
+++ common.js
@@ -0,0 +1,26 @@
+const before = `<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3 p-0">
+            <div class="container-fluid">
+                <img class="navbar-brand" src="777shuang.png" width="64px">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="https://777shuang.github.io/index.html">ホーム</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="https://777shuang.github.io/edufiles/index.html">教育用ファイル</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="https://sites.google.com/view/777shuang/%E3%83%9B%E3%83%BC%E3%83%A0">ぶろぐ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="https://777shuang.github.io/tetris/index.html">なぜか人気のテトリス</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>`
+document.getElementById("main").insertAdjacentHTML("beforebegin",before);
