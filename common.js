f282ed42c89d9292a701926c2856c2f0fc2af16b
777shuang <shuang777@outlook.jp>
2022-11-16 11:12:13 +0900

    Fixed

--- common.js
+++ common.js
@@ -11,7 +11,7 @@ const before = `<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3 p-0
                             <a class="nav-link active" aria-current="page" href="https://777shuang.github.io/index.html">ホーム</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="https://777shuang.github.io/edufiles/index.html">自己紹介</a>
+                            <a class="nav-link" aria-current="page" href="https://777shuang.github.io/aboutme.html">自己紹介</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="https://sites.google.com/view/777shuang/%E3%83%9B%E3%83%BC%E3%83%A0">ぶろぐ</a>
