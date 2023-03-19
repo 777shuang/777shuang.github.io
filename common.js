d47f2c1c9fc205320aa0aaecc7b1076f28c04554
777shuang <shuang777@outlook.jp>
2023-03-19 12:42:17 +0900

    Update

--- common.js
+++ common.js
@@ -8,22 +8,22 @@ var beforestr = `
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="https://777shuang.github.io/index.html">ホーム</a>
+          <a class="nav-link active" aria-current="page" href="/index.html">ホーム</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://scrapbox.io/777shuang/">西方見聞録</a>
+          <a class="nav-link" aria-current="page" href="https://scrapbox.io/777shuang/" target="_blank">西方見聞録</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://777shuang.github.io/blog/index.html">ぶろぐ</a>
+          <a class="nav-link" aria-current="page" href="/blog/index.html">ぶろぐ</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>`;
 var afterstr = `<footer>
-<strong><a href=https://777shuang.github.io/index.html>戻る</a></strong><br><br>
-since : 2022/10/16<br>
-last : 2023/3/13<br>
+<strong><a href=/index.html>戻る</a></strong><br><br>
+<a href="https://sites.google.com/site/happybusy/"><img src="/busy_banner.png"></a>
+since : 2022/10/16 <strong>|</strong> last : 2023/3/19<br>
 <strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong>
 </footer>`
 document.getElementById("main").insertAdjacentHTML("beforebegin",beforestr);
