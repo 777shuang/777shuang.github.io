e25930dcf5db996da6eaa796ed8c15fb7466fbea
777shuang <shuang777@outlook.jp>
2023-03-20 19:07:38 +0900

    Update

--- common.js
+++ common.js
@@ -21,8 +21,9 @@ var beforestr = `
   </div>
 </nav>`;
 var afterstr = `<footer>
-<strong><a href=/index.html>戻る</a></strong><br><br>
-<a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png"></a>
+<strong><a href=/index.html>戻る</a></strong><br>
+<a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64px"></a>
+<a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64px"></a><br>
 since : 2022/10/16 <strong>|</strong> last : 2023/3/19<br>
 <strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong>
 </footer>`
