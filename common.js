6e3d18d21cd4c5a8dfd497106813f702671fbcaa
777shuang <shuang777@outlook.jp>
2023-03-21 14:53:37 +0900

    Update

--- common.js
+++ common.js
@@ -1,31 +1,43 @@
-var beforestr = `
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark mb-3 p-0">
-  <div class="container-fluid">
-    <a href=/logo.html><img class="navbar-brand" src="/assets/777shuang.png" width="64px"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/index.html">ホーム</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="https://scrapbox.io/777shuang/" target="_blank">西方見聞録</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/blog/index.html">ぶろぐ</a>
-        </li>
-      </ul>
+document.getElementById("main").innerHTML = `
+  <div class="window">
+    <div class="title-bar">
+      <div class="title-bar-text">777shuangの気分的ページ</div>
+      <div class="title-bar-controls">
+        <button aria-label="Minimize" onclick="alert('これブラウザだよ?最小化なんてできるわけないじゃん')"></button>
+        <button aria-label="Maximize" onclick="Maximize()" id="titlebar_button"></button>
+        <button aria-label="Close" onclick="window.close()"></button>
+      </div>
     </div>
+` + document.getElementById("main").innerHTML + `
+    <footer>
+      <strong><a href=/index.html>ホームへ戻る</a></strong>
+      <br>
+      <a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64px"></a>
+      <a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64px"></a>
+      <br>
+      since : 2022/10/16 <strong>|</strong> last : 2023/3/19<br>
+      <strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong>
+    </footer>
   </div>
-</nav>`;
-var afterstr = `<footer>
-<strong><a href=/index.html>ホームへ戻る</a></strong><br>
-<a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64px"></a>
-<a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64px"></a><br>
-since : 2022/10/16 <strong>|</strong> last : 2023/3/19<br>
-<strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong>
-</footer>`
-document.getElementById("main").insertAdjacentHTML("beforebegin",beforestr);
-document.getElementById("main").insertAdjacentHTML("afterend",afterstr);
+`
+const main = document.querySelector("#main");
+
+function Maximize()
+{
+  if(!(document.fullscreenEnabled || document.mozFullScreenEnabled || document.documentElement.webkitRequestFullScreen || document.msFullscreenEnabled))
+  { window.alert("フルスクリーンに対応していません"); }
+  else
+  {
+    let reqFullScreen = main.requestFullscreen || main.mozRequestFullScreen || main.webkitRequestFullScreen || main.msRequestFullscreen;
+    reqFullScreen.call(main);
+    document.getElementById("titlebar_button").ariaLabel = "Restore";
+    document.getElementById("titlebar_button").onclick = Restore;
+  }
+}
+
+function Restore()
+{
+  (document.exitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen || document.msExitFullscreen).call(document);
+  document.getElementById("titlebar_button").ariaLabel = "Maximize";
+  document.getElementById("titlebar_button").onclick = Maximize
+}
