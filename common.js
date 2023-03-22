1ad72b281d971ad5e1f795372ab2c9634b60553d
777shuang <shuang777@outlook.jp>
2023-03-22 23:01:37 +0900

    Update

--- common.js
+++ common.js
@@ -17,10 +17,10 @@ body.innerHTML = `
   <footer>
     このサイトは<a href="https://botoxparty.github.io/XP.css/" id="link" target="_blank">XP.css</a>を使って作られています。<button id="changeCSS-button" onclick="changeCSS()">Win98風にする</button>
     <br>
-    <a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64px"></a>
-    <a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64px"></a>
+    <a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64em"></a>
+    <a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64em"></a>
     <br>
-    since : 2022/10/16 <strong>|</strong> last : 2023/3/19<br>
+    since : 2022/10/16 <strong>|</strong> last : 2023/3/22<br>
     <strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong>
   </footer>
 </div>`
