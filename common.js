ac9ee1dcc5cfa6a46de6ee2c674c71dc9f85bd30
777shuang <shuang777@outlook.jp>
2023-03-24 23:16:50 +0900

    Update

--- common.js
+++ common.js
@@ -13,16 +13,16 @@ body.innerHTML = `
   </div>
   <div class="window-body">
 ` + body.innerHTML + `
+    <fieldset>
+      <legend><strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong></legend>
+      このサイトは<a href="https://botoxparty.github.io/XP.css/" id="link" target="_blank">XP.css</a>を使って作られています。<button id="changeCSS-button" onclick="changeCSS()">Win98風にする</button>
+      <br>
+      <a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64em"></a>
+      <a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64em"></a>
+      <br>
+      since : 2022/10/16 <strong>|</strong> last : 2023/3/23<br>
+    </fieldset>
   </div>
-  <footer>
-    このサイトは<a href="https://botoxparty.github.io/XP.css/" id="link" target="_blank">XP.css</a>を使って作られています。<button id="changeCSS-button" onclick="changeCSS()">Win98風にする</button>
-    <br>
-    <a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64em"></a>
-    <a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64em"></a>
-    <br>
-    since : 2022/10/16 <strong>|</strong> last : 2023/3/23<br>
-    <strong>Copyright (c) 2022 - 2023 777shuang. All Rights Reserved.</strong>
-  </footer>
 </div>`
 
 function Maximize(button, element)
