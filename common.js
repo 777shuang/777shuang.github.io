ce9c70afc0a9aa6e8b342553be9f1f598d5dd9ec
777shuang <shuang777@outlook.jp>
2023-07-24 18:44:08 +0900

    2023/7/24

--- common.js
+++ common.js
@@ -20,7 +20,7 @@ body.innerHTML = `
       <a href="https://sites.google.com/site/happybusy/"><img src="/assets/busy_banner.png" height="64em"></a>
       <a href="https://moe-counter.glitch.me/"><img src="https://count.getloli.com/get/@777shuang" height="64em"></a>
       <br>
-      since : 2022/10/16 <strong>|</strong> last : 2023/7/22<br>
+      since : 2022/10/16 <strong>|</strong> last : 2023/7/24<br>
     </fieldset>
   </div>
 </div>`
